ecad3e20432a86b0bb5f12c1a76645601bdab797
elhadis <ahmad23soho@gmail.com>
2021-06-03 11:05:20 +0300

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Galler from './components/gallery/Galler';
+
+function App() {
+  return (
+    <div className="App">
+      <Galler/>
+      
+    </div>
+  );
+}
+
+export default App;
